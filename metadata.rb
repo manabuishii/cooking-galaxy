e6cf008111a2390d82fbcf525b7add8613a5410a
Manabu ISHII <manabu.ishii.rb@gmail.com>
2014-10-20 19:27:42 +0900

    version 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "yo-tanaka@riken.jp"
 license          "Apache 2.0"
 description      "Installs and Configures for galaxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.3.0'
 
 #supports          centos-6.5
 %w{ python mercurial yum vim }.each do |cb|
