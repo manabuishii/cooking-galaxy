94e184515cfcc660c447662282c9372e4a3f4f20
Manabu ISHII <manabu.ishii.rb@gmail.com>
2014-10-21 15:15:24 +0900

    release 0.3.1
    install drmaa as galaxy user

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "yo-tanaka@riken.jp"
 license          "Apache 2.0"
 description      "Installs and Configures for galaxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.3.1'
 
 #supports          centos-6.5
 %w{ python mercurial yum vim }.each do |cb|
